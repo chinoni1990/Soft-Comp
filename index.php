ac4d1e248dc9af7663e925d1587f11f67b4306fb
chinoni1990 <chinoni1990@hotmail.com>
2016-09-13 22:23:45 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Soft-Comp</title>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+	<link href="media/favicon.ico" type="image/x-icon" rel="shortcut icon">
+    <link href="asset/css/normalize.css" type="text/css" rel="stylesheet">
+    <link href="asset/css/bootstrap.css" type="text/css" rel="stylesheet">
+    <link href="asset/css/style.css" type="text/css" rel="stylesheet">
+    <script src="asset/js/jquery.js" type="text/javascript"></script>
+    <script src="asset/js/bootstrap.js" type="text/javascript"></script>
+</head>
+<body>
+    <div class="contenedor-principal">
+        <div class="panel-izquierdo">
+        	<img src="media/Perfil.jpg" class="img-circle img-responsive text-center">
+        </div>
+        <div class="panel-derecho">
+            <div class="bloque  parallax imagen-1">
+                <div class="contenedor">
+                    <h1>Sección 1</h1>
+                </div>
+            </div>
+            <div class="bloque">
+                <div class="contenedor">
+                    <h2>Lorem ipsum </h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div class="bloque   parallax imagen-2 ">
+                <div class="contenedor">
+                    <h1>Sección 2</h1>
+                </div>
+            </div>
+            <div class="bloque">
+                <div class="contenedor">
+                    <h2>Lorem ipsum </h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div class="bloque   parallax imagen-3 ">
+                <div class="contenedor">
+                    <h1>Sección 3</h1>
+                </div>
+            </div>
+            <div class="bloque">
+                <div class="contenedor">
+                    <h2>Lorem ipsum </h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
